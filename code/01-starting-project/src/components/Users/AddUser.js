12b5e2adbcdb01438e0e3c5e8a28185ee35db12f
Noam Ben Shabat <n0556771785@gmail.com>
2022-02-20 22:07:32 +0200

    validate form

--- code/01-starting-project/src/components/Users/AddUser.js
+++ code/01-starting-project/src/components/Users/AddUser.js
@@ -17,19 +17,22 @@ const AddUser = () => {
   };
 
   const addUserHandler = (event) => {
-    console.log(enteredUsername, enteredAge);
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 || +enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' onChange={usernameChangeHandler} />
+        <input type='text' id='username' onChange={usernameChangeHandler} value={enteredUsername} />
         <label htmlFor='age'>Age</label>
-        <input type='number' id='age' onChange={ageChangeHandler}/>
-        <Button type='submit'>
-          Add User
-        </Button>
+        <input type='number' id='age' onChange={ageChangeHandler} value={enteredAge} />
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
